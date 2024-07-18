0acf47614a570db20ff78e3613650babdd035efc
n6teen <note.842545@gmail.com>
2024-07-18 07:16:54 +0700

    update login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Login from './page/login/login';
+import Login from './page/login/loginPatient';
 
 function App() {
   return (
