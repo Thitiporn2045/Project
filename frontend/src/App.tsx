5ac466a50ed2b919d4b246d8cfc30eaaafebd36e
n6teen <note.842545@gmail.com>
2025-01-14 08:03:43 +0700

    ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,7 @@ import PsyCommentBE from './page/psychologist/psyComment/PsyCommentBE';
 import PsyCommentCS from './page/psychologist/psyComment/PsyCommentCS';
 import ListPsycho from './page/admin/listPsycho/listPsycho';
 import ListPat from './page/admin/listPat/listPat';
-import SheetBehav from './page/patient/worksheet/totalSheet/sheetBehav';
+import SheetBehav from './page/patient/worksheet/totalSheet/SheetBehav';
 import FilterEmotions from './component/summaryDiary/filterEmotions';
 
 export default function App() {
